92a568e5489b715a05fcc18366d1002693014b56
MightComeback <kuznetsovivan496@gmail.com>
2024-07-20 20:16:22 +0300

    feat: boilerplate nvim setup

---
+++ init.lua
@@ -0,0 +1,93 @@
+-- NOTE: Settings before require lazy
+
+-- Map leader to <space>
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Use global clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- Set Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Nerd font
+vim.g.have_nerd_font = true
+
+-- Set no line wrap
+vim.o.wrap = false
+
+-- Line numbers
+vim.opt.relativenumber = true
+
+-- Mouse mode
+vim.opt.mouse = 'a'
+
+-- Remove mode show
+vim.opt.showmode = false
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive search by default
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Signcolumn
+vim.opt.signcolumn = 'yes'
+
+-- Splits default behavior
+vim.opt.splitright = ture
+vim.opt.splitbelow = true
+
+-- Whitespace characters configuration
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Live preview substitutions
+vim.opt.inccommand = 'split'
+
+-- Cursor line
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+
+-- Basic keymaps
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+require("config.lazy")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
+	-- Other installed colorschemes
+	{ "vague2k/vague.nvim", name = "vague" },
+	"rktjmp/lush.nvim",
+	"tckmn/hotdog.vim",
+	"dundargoc/fakedonalds.nvim",
+	"craftzdog/solarized-osaka.nvim",
+	{ "rose-pine/neovim", name = "rose-pine" },
+	"eldritch-theme/eldritch.nvim",
+	"jesseleite/nvim-noirbuddy",
+	"vim-scripts/MountainDew.vim",
+	"miikanissi/modus-themes.nvim",
+	"rebelot/kanagawa.nvim",
+	"gremble0/yellowbeans.nvim",
+	"rockyzhang24/arctic.nvim",
+	"folke/tokyonight.nvim",
+	"Shatur/neovim-ayu",
+	"RRethy/base16-nvim",
+	"xero/miasma.nvim",
+	"cocopon/iceberg.vim",
+	"kepano/flexoki-neovim",
+	"ntk148v/komau.vim",
+	{ "catppuccin/nvim", name = "catppuccin" },
+	"uloco/bluloco.nvim",
+	"LuRsT/austere.vim",
+	"ricardoraposo/gruvbox-minor.nvim",
+	"NTBBloodbath/sweetie.nvim",
+	{
+		"maxmx03/fluoromachine.nvim",
+	},
+}
